aa156f626aa66bdc50a19594687e4de2397e3bf2
franckkoukougnon <franckkoukougnon@gmail.com>
2024-07-21 04:03:16 +0200

    first commit of my project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
-
+  text = 'Home';
+  onChangeText() {
+    this.text = 'Nouveau text';
+  }
 }
